66564476102e6ee356916e60ceb426d0c9c358c8
ramasubbaiah <ramasubbaiah@LAPTOP-8FTKGC45>
2022-11-29 21:49:54 +0530

    git

---
+++ src/main/java/DEVOPS/MH5A0506GITO/App.java
@@ -0,0 +1,13 @@
+package DEVOPS.MH5A0506GITO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
